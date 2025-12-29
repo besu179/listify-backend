33d1b950d44ddabdce68ab58ce9b0e706ea225d9
besu178 <besuwinner@gmail.com>
2025-12-29 14:35:38 +0300

    Implement social graph functionality with follow/unfollow features and user relationships

--- app/controllers/api/v1/auth/sessions_controller.rb
+++ app/controllers/api/v1/auth/sessions_controller.rb
@@ -4,13 +4,6 @@ module Api
       class SessionsController < Devise::SessionsController
         respond_to :json
 
-        def create
-          self.resource = warden.authenticate!(auth_options)
-          sign_in(resource_name, resource, store: false)
-          yield resource if block_given?
-          respond_with(resource, location: after_sign_in_path_for(resource))
-        end
-
         private
 
         def respond_with(resource, _opts = {})
